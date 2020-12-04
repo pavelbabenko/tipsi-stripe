d64a09a2b3dd80c2f3db3a79752f8ff18d4d30fb
cYee992 <cYee992@live.com>
2020-12-04 19:37:20 +0800

    remove id

--- android/src/main/java/com/gettipsi/stripe/CreditCardFormOnChangeEvent.java
+++ android/src/main/java/com/gettipsi/stripe/CreditCardFormOnChangeEvent.java
@@ -10,30 +10,31 @@ import com.facebook.react.uimanager.events.RCTEventEmitter;
  */
 public class CreditCardFormOnChangeEvent extends Event<CreditCardFormOnChangeEvent> {
 
-    private static final String EVENT_NAME = "topChange";
-    private final WritableMap params;
-    private final boolean isValid;
+  private static final String EVENT_NAME = "topChange";
+  private final WritableMap params;
+  private final boolean isValid;
 
-    public CreditCardFormOnChangeEvent(int id, WritableMap params, boolean isValid) {
-        super(id);
-        this.params = params;
-        this.isValid = isValid;
-    }
+  public CreditCardFormOnChangeEvent(int id, WritableMap params, boolean isValid) {
+    // super(id);
+    super();
+    this.params = params;
+    this.isValid = isValid;
+  }
 
-    @Override
-    public String getEventName() {
-        return EVENT_NAME;
-    }
+  @Override
+  public String getEventName() {
+    return EVENT_NAME;
+  }
 
-    @Override
-    public void dispatch(RCTEventEmitter rctEventEmitter) {
-       // rctEventEmitter.receiveEvent(getViewTag(), getEventName(), serializeEventData());
-    }
+  @Override
+  public void dispatch(RCTEventEmitter rctEventEmitter) {
+    rctEventEmitter.receiveEvent(getViewTag(), getEventName(), serializeEventData());
+  }
 
-    private WritableMap serializeEventData() {
-        WritableMap eventData = Arguments.createMap();
-        eventData.putBoolean("valid", isValid);
-        eventData.putMap("params", params);
-        return eventData;
-    }
+  private WritableMap serializeEventData() {
+    WritableMap eventData = Arguments.createMap();
+    eventData.putBoolean("valid", isValid);
+    eventData.putMap("params", params);
+    return eventData;
+  }
 }
